103f6e196ef56275778806d9dc43c0b40605125e
salamk195 <abdulsalamawosanya5@gmail.com>
2024-06-24 19:16:54 +0100

    assignment 1.py

--- assignment 1.py
+++ assignment 1.py
@@ -1,1 +1,47 @@
+"""Mr and Mrs Bose are couples, Mrs Bose needs to create a monthly budjets and Mr Bose who is a python programmer helps her using python coding"""
+rent = 1200
+groceries = 3000
+transportation = 150 
+entertainment = 100 
+miscellaneous = 300
+"""calculate the monthly expenses."""
+monthly_expenses = rent + groceries + entertainment + miscellaneous +transportation
+print(monthly_expenses)
+daily_expenses = monthly_expenses / 30
+print(daily_expenses)
 
+
+
+
+
+
+
+
+
+
+
+"""Temperature_converter"""
+"""if c = 65,find f"""
+c = 65 
+f = c * 9/5 + 32
+print(f)
+
+
+
+
+
+"""age calculator"""
+"""if birth year is 2007,find age."""
+current_year = 2024
+birth_year = 2007
+age = current_year - birth_year
+print(age)
+
+
+
+
+"""the mean of 1,2,3,4,5"""
+sum_of_numbers = 1 + 2 + 3 + 4 + 5
+counted_numbers = 5
+mean = sum_of_numbers / counted_numbers
+print(mean)
