3badc8cbc67b1e9aa7e2036a1051696fb302ea7c
salamk195 <abdulsalamawosanya5@gmail.com>
2024-07-07 16:54:50 +0100

    Quiz.py

---
+++ Quiz.py
@@ -0,0 +1,98 @@
+Question1 = int(input("4 + 1?"))
+if Question1 == 5:
+    print("correct")
+    if Question1 == 5:
+        totalscore += 1
+else:
+    print("wrong")
+
+
+
+Question2 = int(input("5 + 7?"))
+if Question2 == 12:
+    print("correct")
+    if Question2 == 12:
+        totalscore += 1
+else:
+    print("wrong")
+
+
+
+Question3 = int(input("6 + 4?"))
+if Question3 == 10:
+    print("correct")
+    if Question3 == 10:
+        totalscore += 1
+else:
+    print("wrong")
+
+
+
+Question4 = int(input("3 + 1?"))
+if Question4 == 4:
+    print("correct")
+    if Question4 == 4 :
+        totalscore += 1
+else:
+    print("wrong")
+
+
+
+Question5= int(input("6 + 7?"))
+if Question5 == 13:
+    print("correct")
+    if Question5 == 13:
+        totalscore += 1
+else:
+    print("wrong")
+
+
+
+Question6 = int(input("3 + 9?"))
+if Question6 == 12 :
+    print("correct")
+    if Question1 == 12:
+        totalscore += 1
+else:
+    print("wrong")
+
+
+
+Question7 = int(input("4 + 9?"))
+if Question7 == 13:
+    print("correct")
+    if Question7 == 13:
+        totalscore += 1
+else:
+    print("wrong")
+
+
+
+Question8 = int(input("4 + 2?"))
+if Question8 == 6:
+    print("correct")
+    if Question8 == 6:
+        totalscore += 1
+else:
+    print("wrong")
+
+
+
+Question9 = int(input("4 + 13?"))
+if Question9 == 17:
+    print("correct")
+    if Question9 == 17:
+        totalscore += 1
+else:
+    print("wrong")
+
+
+
+Question10 = int(input("4 + 20?"))
+if Question10 == 24:
+    print("correct")
+    if Question10 == 24:
+        totalscore += 1
+else:
+    print("wrong")
+print("Your totalscore is " + totalscore)
